0e15ff9d9eeff4522ab3bb19fe5990554baa6c5b
Krishna Chaitanya <stealgamerofficial@gmail.com>
2024-05-14 20:35:33 +0530

    added locomotive js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,15 @@
-// gsap.from("#logo",{
-//     y: -50,
-//     delay: 0.5,
-//     duration: 1
-// })
+gsap.from("#lpic",{
+    x: -180,
+    delay: 0.5,
+    duration: 1
+});
 
-// gsap.from("#elems", {
-//     x: -20,
-//     opacity: 0,
-//     duration: 1
-// })
+gsap.to("#lbox", {
+    x: -100,
+    opacity: 0,
+    duration: 1.5,
+    delay: 5
+});
 
 
 const scroll = new LocomotiveScroll({
